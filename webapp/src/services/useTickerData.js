abb4b9bec15612a77a6e0aa6e734badea27f5094
daner06 <daner06@gmail.com>
2022-09-18 10:13:29 +0100

    CHA-1 - Fixed gql data issue and improved layout

--- webapp/src/services/useTickerData.js
+++ webapp/src/services/useTickerData.js
@@ -1,28 +1,12 @@
-import React, { useEffect, useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { HISTORY_DATA } from './graphQL/queries';
-import config from '../config/config';
 
 const useTickerData = (ticker, timeFrame) => {
-  const [newData, setNewData] = useState(null);
   const { loading, error, data } = useQuery(HISTORY_DATA, {
     variables: { timeFrame },
-    pollInterval: config.pollInterval,
   });
 
-  useEffect(() => {
-    if (data !== undefined) {
-      const myNewData = data.marketData.filter(d => d.timeFrame === timeFrame).map(d => {
-        return {
-          yLabel: d.yLabel,
-          value: d.value,
-        };
-      });
-      setNewData(myNewData);
-    }
-  }, [data]);
-
-  return { data: newData, loading, error };
+  return { data, loading, error };
 };
 
 export default useTickerData;
--- webapp/src/views/App.js
+++ webapp/src/views/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import { Button, ButtonGroup } from 'react-bootstrap';
 import Chart from './components/Chart';
 import { timeFrames } from './timeFrames';
 import '../App.css';
@@ -6,44 +8,42 @@ import config from '../config/config';
 import useTickerData from '../services/useTickerData';
 
 const App = () => {
+  const { ticker, tickerName } = config;
   const [timeFrame, setTimeFrame] = useState(timeFrames[0].value);
-  const { data, loading, error } = useTickerData(config.ticker, timeFrame);
+  const { data, loading, error } = useTickerData(ticker, timeFrame);
 
   return <div className="App">
-    <header className="App-header">
-      <p>
-        Coding challenge 🚀 - TotalEnergies share price
-      </p>
+    <Container className="p-3">
+      <h3>
+        Coding challenge 🚀 - {tickerName} share price
+      </h3>
       <div>
         {loading ?
           <p>Loading...</p>
           : error ?
             <p>Error {error?.message}</p>
             : <div>
-              <p>{timeFrame}</p>
-              <div className="frame-selector">
-                {timeFrames.map((item, index) => {
-                  return <button
-                    key={index.toString()}
-                    className="time-frame"
-                    type="button"
-                    onClick={() => setTimeFrame(item.value)}
-                  >
-                    {item.label}
-                  </button>
-                })}
-              </div>
+              {timeFrames.map((item, index) => {
+                return <Button
+                  key={index.toString()}
+                  className="time-frame-button"
+                  size="sm"
+                  type="button"
+                  onClick={() => setTimeFrame(item.value)}
+                >
+                  {item.label}
+                </Button>
+              })}
               <Chart
                 timeFrame={timeFrame}
-                title={`Market data for ${config.ticker} - ${timeFrame}`}
-                serielabel={config.ticker}
+                title={`Market data for ${ticker} - ${timeFrame}`}
+                serielabel={ticker}
                 data={data}
               />
             </div>
         }
       </div>
-
-    </header>
+    </Container>
   </div>
 };
 
